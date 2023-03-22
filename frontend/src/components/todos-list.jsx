584616cf27501ca3308a8b7f96684a195b4059ad
anthonynarine <anarine83@gmail.com>
2023-03-21 20:34:54 -0400

    Listing todo's for logged in users completed

--- frontend/src/components/todos-list.jsx
+++ frontend/src/components/todos-list.jsx
@@ -1,57 +1,79 @@
-import { Typography, Grid, Paper, Box, Card, CardMedia, CardContent, CardActions, Button } from "@mui/material";
-import TodoDataService from "../services/axios-requests"
-import {useState, useEffect} from "react"
+import {
+  Typography,
+  Grid,
+  Paper,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+} from "@mui/material";
+import TodoDataService from "../services/axios-requests";
+import { useState, useEffect } from "react";
 
-function TodoList({token}) {
-
+function TodoList({ token }) {
   const [todos, setTodos] = useState([]);
-  
-  useEffect(()=>{
-    retrieveTodos();
-   
-  },[token] );
 
-  const retrieveTodos = ()=> {
+  useEffect(() => {
+    retrieveTodos();
+  }, [token]);
+
+  const retrieveTodos = () => {
     TodoDataService.getAll(token)
-    .then(response =>{
-      setTodos(response.data);
-      console.log("Data", response.data)
-    })
-    .catch(error =>{
-      console.log(error);
-    })
-  }
+      .then((response) => {
+        setTodos(response.data);
+        console.log("Data", response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <Paper>
-      <Grid justifyContent="space-evenly" container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}></Grid>
+    <Paper
+      elevation={24}
+      sx={{
+        padding: 1,
+        width: 390,
+        margin: "auto",
+        marginTop: 1.5,
+        backgroundColor: "#0F0616",
+      }}
+    >
       <Grid item xs={8}>
-        {todos.map((todo)=>{
-          return(
-            <Card sx={{ maxWidth: 345, backgroundColor: "#AEBDC7" }}>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {todo.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                <span style={{fontWeight:"bold"}} >Memo:</span> {todo.memo}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-              <span style={{fontWeight:"bold"}} >Date Created:</span> {todo.created}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Edit</Button>
-              <Button size="small">Delete</Button>
-            </CardActions>
-          </Card>
-          )
+        {todos.map((todo) => {
+          return (
+            <Card
+              sx={{
+                maxWidth: 345,
+                backgroundColor: "white",
+                margin: "auto",
+                marginTop: 2,
+                marginBottom: 2,
+              }}
+            >
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {todo.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <span style={{ fontWeight: "bold" }}>Memo:</span> {todo.memo}
+                </Typography>
+                <Typography sx={{marginTop:3}} variant="body2" color="text.secondary">
+                  <span style={{ fontWeight: "bold" }}>Date Created:</span>{" "}
+                  {todo.created}
+                </Typography>
+              </CardContent>
+              <CardActions  sx={{marginTop:-3}}>
+                <Button sx={{marginLeft: 10}} size="small">Edit</Button>
+                <Button size="small">Delete</Button>
+              </CardActions>
+            </Card>
+          );
         })}
-
-    </Grid>
-
+      </Grid>
     </Paper>
-
   );
 }
 
